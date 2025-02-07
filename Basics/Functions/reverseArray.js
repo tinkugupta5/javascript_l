a5b1aeb814222856195acea5a81499d779ccdc0d
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-08 01:23:02 +0530

    [08-02-2025]-reverse-the-array

---
+++ Basics/Functions/reverseArray.js
@@ -0,0 +1,14 @@
+const reverseArray = (arr) => {
+    const reversedArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArr.push(arr[i]);
+    }
+    return reversedArr;
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+console.log(reversedArray); 
+
+// reverses the array Done
+
