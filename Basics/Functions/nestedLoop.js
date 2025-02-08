aec0b045b84e08c45aaa5fceb0ee30ced9414910
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-09 01:05:40 +0530

    nestedLoop

--- Basics/Functions/nestedLoop.js
+++ Basics/Functions/nestedLoop.js
@@ -2,8 +2,10 @@ function printNumberPattern(rows) {
     for (let i = 1; i <= rows; i++) {
         let row = '';
         for (let j = 1; j <= i; j++) {
-            row += j + ' ';
+            
+            row = row + j + ' ';
         }
+
         console.log(row);
     }
 }
