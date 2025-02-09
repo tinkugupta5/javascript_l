ccac1b99df24b60ac5984eba98af7230df170742
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-10 00:36:44 +0530

    Multipliation method 2nd

--- Basics/Functions/PrintNumber.js
+++ Basics/Functions/PrintNumber.js
@@ -5,3 +5,14 @@ for (let i = 1; i <= 10; i++) {
 }
 
 
+
+// console.log("Multiplication Table (up to 10)");
+
+// for (let i = 1; i <= 10; i++) {  
+//   let rowOutput = ""; 
+
+//   for (let j = 1; j <= 10; j++) { 
+//     rowOutput += (i * j) + "\t"; 
+//   }
+//   console.log(rowOutput); 
+// }
