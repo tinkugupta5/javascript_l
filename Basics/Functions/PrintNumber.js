a5eba333ea40d05f18e72e612988f22344828ae8
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-11 01:39:10 +0530

    duplicate remove item from an array

--- Basics/Functions/PrintNumber.js
+++ Basics/Functions/PrintNumber.js
@@ -1,18 +1,6 @@
+
 console.log("\n2 Times Table:");
 
 for (let i = 1; i <= 10; i++) {
-  console.log("4 x " + i + " = " + (4 * i));
-}
-
-
-
-// console.log("Multiplication Table (up to 10)");
-
-// for (let i = 1; i <= 10; i++) {  
-//   let rowOutput = ""; 
-
-//   for (let j = 1; j <= 10; j++) { 
-//     rowOutput += (i * j) + "\t"; 
-//   }
-//   console.log(rowOutput); 
-// }
+  console.log("2 x "  i  " = "  (2 * i));
+}  
---
+++ Basics/Functions/duplicates.js
@@ -0,0 +1,20 @@
+function removeDuplicate(arr) {
+    const uniqueArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let isDuplicate = false;
+        for (let j = 0; j < uniqueArr.length; j++) {
+            if (arr[i] === uniqueArr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    return uniqueArr;
+}
+
+const originalArray = [1, 2, 3, 4, 4, 5];
+const removedDuplicate = removeDuplicate(originalArray);
+console.log(removedDuplicate); // Output: [1, 2, 3, 4, 5]
