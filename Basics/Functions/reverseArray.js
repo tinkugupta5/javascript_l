b9ad08c92cbfb4b4a3e3b95b6f0d28fb97838493
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-12 23:48:27 +0530

    chnages in reverse array

--- Basics/Functions/reverseArray.js
+++ Basics/Functions/reverseArray.js
@@ -6,6 +6,7 @@ const reverseArray = (arr) => {
     return reversedArr;
 }
 
+
 const originalArray = [1, 2, 3, 4, 5];
 const reversedArray = reverseArray(originalArray);
 console.log(reversedArray); 
