9ff50efac3b011a56fcb0d4148d78b6788564d2f
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-13 23:20:32 +0530

    duplicate values

--- Basics/Functions/duplicatevalue.js
+++ Basics/Functions/duplicatevalue.js
@@ -4,6 +4,7 @@ function findDuplicates(arr) {
     for (let i = 0; i < arr.length; i++) {
         let count = 0;
         
+        
         // Check how many times arr[i] appears in arr
         for (let j = 0; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
