677a269c17792f6c9d05d0e06698de59f7ac8f7e
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-16 23:06:20 +0530

    remove the comments

--- Basics/Functions/duplicatevalue.js
+++ Basics/Functions/duplicatevalue.js
@@ -1,18 +1,12 @@
 function findDuplicates(arr) {
     let duplicates = [];
-    
     for (let i = 0; i < arr.length; i++) {
         let count = 0;
-        
-        
-        // Check how many times arr[i] appears in arr
         for (let j = 0; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
                 count++;
             }
         }
-        
-        // If count > 1, and it's not already in duplicates, add it
         let isAlreadyAdded = false;
         for (let k = 0; k < duplicates.length; k++) {
             if (duplicates[k] === arr[i]) {
