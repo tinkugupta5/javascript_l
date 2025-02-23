a06fa1af550e00b93210791139e6d8f562668591
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-24 00:15:14 +0530

    chnages in duplicates programs

--- Basics/Functions/duplicates.js
+++ Basics/Functions/duplicates.js
@@ -1,19 +1,13 @@
 function removeDuplicate(arr) {
     const uniqueArr = [];
     for (let i = 0; i < arr.length; i++) {
-        let isDuplicate = false;
-        for (let j = 0; j < uniqueArr.length; j++) {
-            if (arr[i] === uniqueArr[j]) {
-                isDuplicate = true;
-                break;
-            }
-        }
-        if (!isDuplicate) {
-            uniqueArr.push(arr[i]);
-        }
-    } 
+      if (!uniqueArr.includes(arr[i])) {
+        uniqueArr.push(arr[i]);
+      }
+    }
     return uniqueArr;
-}
-const originalArray = [1, 2, 3, 4, 4, 5];
-const removedDuplicate = removeDuplicate(originalArray);
-console.log(removedDuplicate); 
+  }
+  
+  const originalArray = [1, 2, 3, 4, 4, 5];
+  const removedDuplicate = removeDuplicate(originalArray);
+  console.log(removedDuplicate); // Output: [1, 2, 3, 4, 5]
