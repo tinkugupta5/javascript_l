d4ffff7a5bd66bb3f9ef861abb5f923607ab7480
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-23 23:27:02 +0530

    intersection and union

---
+++ Basics/Functions/intersectiontwo.js
@@ -0,0 +1,16 @@
+function findIntersection(arr1, arr2) {
+    const set1 = new Set(arr1);
+    console.log(set1);
+
+    const set2 = new Set(arr2);
+    console.log(set2);
+
+    return [...set1].filter(value => set2.has(value));
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+const result = findIntersection(array1, array2);
+// const result2 = findIntersection(array1,array2);
+console.log(result); 
+// Output: [4, 5]
