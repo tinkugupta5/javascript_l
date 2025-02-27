3c4c4ca33823da3a0e005c5608d79324f2d42784
tinkugupta5 <tinku.gupta4@gmail.com>
2025-02-28 01:30:07 +0530

    intersection array

--- Basics/Functions/intersectiontwo.js
+++ Basics/Functions/intersectiontwo.js
@@ -11,6 +11,5 @@ function findIntersection(arr1, arr2) {
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [4, 5, 6, 7, 8];
 const result = findIntersection(array1, array2);
-// const result2 = findIntersection(array1,array2);
 console.log(result); 
 // Output: [4, 5]
