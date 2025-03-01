00668d0c4a253d35a6045ba73e1e29b630215d62
tinkugupta5 <tinku.gupta4@gmail.com>
2025-03-01 23:30:47 +0530

    duplicate

--- Basics/Functions/duplicates.js
+++ Basics/Functions/duplicates.js
@@ -10,4 +10,4 @@ function removeDuplicate(arr) {
   
   const originalArray = [1, 2, 3, 4, 4, 5];
   const removedDuplicate = removeDuplicate(originalArray);
-  console.log(removedDuplicate); // Output: [1, 2, 3, 4, 5]
+  console.log(removedDuplicate); //Output: [1, 2, 3, 4, 5]
