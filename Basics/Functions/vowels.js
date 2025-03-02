287b15578acde741ecd7f4d555ad778f35b1775b
tinkugupta5 <tinku.gupta4@gmail.com>
2025-03-03 00:48:24 +0530

    vowels chnages

--- Basics/Functions/vowels.js
+++ Basics/Functions/vowels.js
@@ -1,7 +1,6 @@
 function countVowels(str) {
     const vowels = 'aeiouAEIOU';
     let count = 0;
-
     for (let i = 0; i < str.length; i++) {
         if (vowels.includes(str[i])) {
             count++;
