465474663bb8ae300b8cc360fab9cddd43167bc6
tinkugupta5 <tinku.gupta4@gmail.com>
2025-03-19 01:05:08 +0530

    chnages in remove duplicate

--- Basics/Functions/secondLargest.js
+++ Basics/Functions/secondLargest.js
@@ -17,3 +17,35 @@ function secondLargest(arr) {
 console.log(secondLargest([10, 20, 4, 45, 99])); 
 console.log(secondLargest([5, 5, 5, 5])); 
 console.log(secondLargest([3])); 
+
+
+// for adobe 
+
+// tech stack round 
+
+
+// function curring 
+// closures
+
+
+// csr ssr
+// how react works under the hood
+// webpacks
+// redux related questions
+// different approces to create react approces
+
+
+// data structure basic to intermediate level
+
+// machine round 
+
+// To do app i have to optimize in react js
+
+// use context and usereducer 
+
+// low level design 
+
+// system design 
+// debounce and throtling
+
+
