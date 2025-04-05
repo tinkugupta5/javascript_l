04bb1a9a7e080aa11daed20dbf238c9c17c1f39c
tinkugupta5 <tinku.gupta4@gmail.com>
2025-04-05 23:56:39 +0530

    reversearray

--- Basics/Functions/reverseArray.js
+++ Basics/Functions/reverseArray.js
@@ -5,8 +5,6 @@ const reverseArray = (arr) => {
     }
     return reversedArr;
 }
-
-
 const originalArray = [1, 2, 3, 4, 5];
 const reversedArray = reverseArray(originalArray);
 console.log(reversedArray); 
