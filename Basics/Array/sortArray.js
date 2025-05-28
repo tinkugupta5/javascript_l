f01804346457ec629e4a008c947643b89f64b5ce
tinkugupta5 <tinku.gupta4@gmail.com>
2025-05-28 23:49:56 +0530

    sortArray

--- Basics/Array/sortArray.js
+++ Basics/Array/sortArray.js
@@ -19,11 +19,12 @@ function sortArray(arr){
             if(arr[j] >arr[j+1]){
                 let temp = arr[j]
                 arr[j] = arr[j+1];
-                
-
+                arr[j+1] = temp;
             }
         }
     }
+
+    return arr
 }
 
 
--- Basics/Pattern/Pattern1.js
+++ Basics/Pattern/Pattern1.js
@@ -10,3 +10,4 @@ function reverseArray(arr) {
 const original = [1, 2, 3, 4, 5];
 const reversedArray = reverseArray(original);
 console.log(reversedArray);
+
