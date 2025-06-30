387716e26c4edee561de008f8ef4a0e2a381db93
tinkugupta5 <tinku.gupta4@gmail.com>
2025-06-30 15:48:05 +0530

    chnages in twoSum  code

--- Basics/Array/twoSum.js
+++ Basics/Array/twoSum.js
@@ -5,13 +5,7 @@ function twoSumfn(arr,target)
     console.log(target)
     for(let i = 0; i<arr.length;i++)
     {
-        for(let j = i+1; j<arr.length;j++)
-        {
-            if(arr[i]+arr[j] === target)
-            {
-                return [i,j]
-            }
-        } 
+        
     }
     return null ;
 }
@@ -20,11 +14,7 @@ const originalArray = [2,8,11,15];
 const target = 9;
 
 const twoSumAns = twoSumfn(originalArray,target)
-
-
-
 const originalArraydata  = [1,2,3,4,5,5,6];
-const tragetValue = 9;
 
 
 
