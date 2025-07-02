3f39d390e8b5d5e95ee6a0e237ab75d43a97cda2
tinkugupta5 <tinku.gupta4@gmail.com>
2025-07-02 23:30:18 +0530

    num i and num j comparision

--- Basics/Array/twoSum.js
+++ Basics/Array/twoSum.js
@@ -1,14 +1,14 @@
-
-function twoSumfn(arr,target)
-{
-    console.log(arr)
-    console.log(target)
-    for(let i = 0; i<arr.length;i++)
-    {
-        
+function twoSumfn(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-    return null ;
+  }
+  return [];
 }
+
 
 const originalArray = [2,8,11,15];
 const target = 9;
