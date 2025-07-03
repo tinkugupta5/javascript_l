a72e6ec8bd4798647d3aa947d4f9d10b18faf9be
tinkugupta5 <tinku.gupta4@gmail.com>
2025-07-03 23:45:55 +0530

    flatternArray new way

---
+++ Basics/Array/flatternArray.js
@@ -0,0 +1,21 @@
+
+function flatternArray(arr) {
+    let emptyArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            emptyArray = emptyArray.concat(flatternArray(arr[i]))
+        }
+        else {
+            emptyArray.push(arr[i])
+        }
+    }
+
+    return emptyArray
+}
+
+
+
+const originalArray = [1, [2, [3, [4, [5]]]]]
+const result = flatternArray(originalArray);
+console.log(result)
