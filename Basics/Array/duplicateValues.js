9fc1db543bf2d52bab268ca923ff07f13bea240c
tinkugupta5 <tinku.gupta4@gmail.com>
2025-07-05 23:23:54 +0530

    duplicatevalues

--- Basics/Array/duplicateValues.js
+++ Basics/Array/duplicateValues.js
@@ -1,7 +1,10 @@
-const arr = [1, 2, 3, 4, 2, 3, 5];
+
 const duplicates = arr.filter((element,index) => {
     return arr.indexOf(element) !== index
 })
+
+
+const arr = [1, 2, 3, 4, 2, 3, 5];
 
 console.log(duplicates);
  
