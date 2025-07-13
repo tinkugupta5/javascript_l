8920c821167bccedc6c1ae06d13505c60a22fafb
tinkugupta5 <tinku.gupta4@gmail.com>
2025-07-13 15:39:07 +0530

    two some chnages in the code

--- Basics/Array/removeDuplicatesArray.js
+++ Basics/Array/removeDuplicatesArray.js
@@ -1,6 +1,6 @@
 function removeDuplicate(arr) {
+   let uniqueArr = [];
     for (let i = 0; i < arr.length; i++) {
-      let uniqueArr = [];
       if (!uniqueArr.includes(arr[i])) {
         uniqueArr.push(arr[i]);
       }
--- Basics/Array/twoSum.js
+++ Basics/Array/twoSum.js
@@ -1,7 +1,7 @@
-function twoSumfn(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
+function twoSumfn(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
         return [i, j];
       }
     }
@@ -14,6 +14,11 @@ const originalArray = [2,8,11,15];
 const target = 9;
 const twoSumAns = twoSumfn(originalArray,target)
 const originalArraydata  = [1,2,3,4,5,5,6];
+
+
+// 
+
+// return mistake i have return like this arr[i,j]
 
 
 
