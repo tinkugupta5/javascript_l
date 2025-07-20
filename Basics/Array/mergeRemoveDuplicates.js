4a46fdc2d1bb8d583c5b32094b93925989a3ee88
tinkugupta5 <tinku.gupta4@gmail.com>
2025-07-20 23:42:10 +0530

    mergedArray

--- Basics/Array/mergeRemoveDuplicates.js
+++ Basics/Array/mergeRemoveDuplicates.js
@@ -2,7 +2,6 @@
 function mergedArrayandRemoveDuplicates(arr1, arr2) {
     return [...new Set([...arr1, ...arr2])];
 }
-
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [6, 7, 8, 9, 10];
 const mergedArray = mergedArrayandRemoveDuplicates(array1, array2);
