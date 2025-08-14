6bb7808c0e8b1767cc942e42582b386539f4596d
tinkugupta5 <tinku.gupta4@gmail.com>
2025-08-14 14:10:05 +0530

    sortArray

--- Basics/Array/sortArray.js
+++ Basics/Array/sortArray.js
@@ -1,12 +1,11 @@
 
-function sortArray(arr){
-    for(let i =0; i<arr.length;i++){
-        for(let j =0; j<arr.length-1;j++)
-        {
-            if(arr[j] >arr[j+1]){
+function sortArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 let temp = arr[j]
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -18,3 +17,6 @@ function sortArray(arr){
 const originalArray = [1, 6, 3, 5, 4, 2]
 const sortedArray = sortArray(originalArray);
 console.log(sortedArray);
+
+
+267ee9ed194c3249859042259c051d12
