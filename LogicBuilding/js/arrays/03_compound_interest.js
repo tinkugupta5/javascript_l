689062d8326d45859cb820bf81547a24628afa6e
tinkugupta5 <tinku.gupta4@gmail.com>
2026-03-31 01:51:37 +0530

    two in python

--- LogicBuilding/js/arrays/03_compound_interest.js
+++ LogicBuilding/js/arrays/03_compound_interest.js
@@ -5,5 +5,4 @@ const T = 2;
 
 const A = P * Math.pow(1 + R / 100, T);
 const CI = A - P;
-
 console.log("Compound interest:", CI);
---
+++ LogicBuilding/python/04_twosum.py
@@ -0,0 +1,17 @@
+def two_sum(nums, target):
+    i = 0
+    
+    for num1 in nums:
+        j = i + 1
+        
+        for num2 in nums[i+1:]:
+            if num1 + num2 == target:
+                return [i, j]
+            j += 1
+        
+        i += 1
+            
+nums = [2, 7, 11, 15]
+target = 9
+
+print(two_sum(nums, target))
