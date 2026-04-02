16315f7a7e3ac769b8b9ca1234199531954164b5
tinkugupta5 <tinku.gupta4@gmail.com>
2026-04-03 00:40:12 +0530

    add two sum problem

---
+++ LogicBuilding/js/arrays/04_twosumproblem.js
@@ -0,0 +1,15 @@
+function twoSumfn(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+const originalArray = [2, 8, 11, 15];
+const target = 10;
+const twoSumAns = twoSumfn(originalArray, target);
+console.log("Brute force result:", twoSumAns); 
