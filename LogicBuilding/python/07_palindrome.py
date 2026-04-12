5eabd62b87878fca8b4c524c355d221adbd9f4d0
tinkugupta5 <tinku.gupta4@gmail.com>
2026-04-12 22:26:32 +0530

    palindrome program

---
+++ LogicBuilding/python/07_palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(text):
+    cleaned = ''.join(ch.lower() for ch in text if ch.isalnum())
+    return cleaned == cleaned[::-1]
+
+sample_text = "A man, a plan, a canal, Panama"
+if is_palindrome(sample_text):
+    print(f'"{sample_text}" is a palindrome')
+else:
+    print(f'"{sample_text}" is not a palindrome')
